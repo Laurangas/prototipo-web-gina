2007aef5bfd0fe20bc689eac2abe5480f179c7d6
Rodo <rodogonzalez98@gmail.com>
2020-06-25 21:36:28 -0500

    Añadido servidor web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBUreebHWUsP_GcLz8eCo5J7rPNpFeWxHQ",
+    authDomain: "testdb1562.firebaseapp.com",
+    databaseURL: "https://testdb1562.firebaseio.com",
+    projectId: "testdb1562",
+    storageBucket: "testdb1562.appspot.com",
+    messagingSenderId: "857568232808",
+    appId: "1:857568232808:web:15915ec5c4f363ab2302dd"
+  }
 };
