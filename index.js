a259a1faad261377055ef2f893956edb90f250b9
bernardo97 <bolongo_berna@hotmail.com>
2020-06-30 01:28:13 -0500

    Agregado codigo QR

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const port = 4200;
 const app = express();
+const fs=require("fs");
+const qrcode=require("qrcode");
 
+const urlCv="https://automaga.umizoomi.tk";
+
+const run=async()=> {
+  const QR=await qrcode.toDataURL(urlCv)
+  const htmlContent=`
+  <div style="display: flex; justify-content: center; align-items: center;">
+  <h2>QR Umizumi</h2>
+  <img src="${QR}">
+  </div>
+  `;
+  fs.writeFileSync('./qra.html',`${htmlContent}`)
+}
+
+run()
 app.use(cors());
 
 app.use(express.static('dist'));
