169af7f8d628f60b5f4d2c78ca57b1e8b05e3245
Rodo <rodogonzalez98@gmail.com>
2020-07-09 01:54:30 -0500

    Added cancel

--- src/app/services/reader/reader.service.ts
+++ src/app/services/reader/reader.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationEnd, Event as NavigationEvent } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReaderService {
 
-  constructor() { }
-
   Text = '';
   synth = window.speechSynthesis;
   rep: SpeechSynthesisUtterance;
   pausado = false;
+
+  constructor(router: Router) { 
+    router.events
+    .subscribe(
+      (event: NavigationEvent) => {
+        if (event instanceof NavigationEnd) {
+          this.synth.cancel();
+        }
+      });
+  }
 
   reproducir() {
     if ( this.synth.paused ) {
